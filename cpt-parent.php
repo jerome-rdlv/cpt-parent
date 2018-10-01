52f233e55b255f337a17ce9595f765a7d132514a
Jérôme Mulsant <jerome@rue-de-la-vieille.fr>
2018-10-01 11:21:12 +0200

    First commit

---
+++ cpt-parent.php
@@ -0,0 +1,253 @@
+<?php
+
+CptParent::init();
+
+class CptParent
+{
+    const OPTION_FORMAT = 'page_for_%s';
+    
+    private static $instances = array();
+    private static $post_type_args = array();
+    
+    public static function init()
+    {
+        load_muplugin_textdomain('cpt-parent');
+
+        // save original post type args and update it
+        add_filter('register_post_type_args', function ($args, $post_type) {
+            if (apply_filters('cpt_has_parent_page', false, $post_type)) {
+                self::$post_type_args[$post_type] = $args;
+                $parent = get_option(sprintf(self::OPTION_FORMAT, $post_type));
+                if ($parent) {
+                    $args['rewrite']['slug'] = get_page_uri($parent);
+                    $args['rewrite']['parent'] = $parent;
+                }
+            }
+            return $args;
+        }, 10, 2);
+
+        add_action('registered_post_type', function ($post_type, WP_Post_Type $post_type_object) {
+            if (apply_filters('cpt_has_parent_page', false, $post_type)) {
+                if (!array_key_exists($post_type, self::$instances)) {
+                    self::$instances[$post_type] = new CptParent($post_type, $post_type_object);
+                }
+            }
+        }, 10, 2);
+
+        // transform breadcrumbs
+        add_filter('wpseo_breadcrumb_links', function ($crumbs) {
+            foreach ($crumbs as $index => &$crumb) {
+                if (!array_key_exists('ptarchive', $crumb)) {
+                    continue;
+                }
+                $post_type = $crumb['ptarchive'];
+                if (!array_key_exists($post_type, self::$instances)) {
+                    continue;
+                }
+                $post_type_obj = get_post_type_object($post_type);
+                if (empty($post_type_obj->rewrite['parent'])) {
+                    continue;
+                }
+                $parent = $post_type_obj->rewrite['parent'];
+                $parent_post = get_post($parent);
+                unset($crumb['ptarchive']);
+                $crumb['id'] = $parent_post->ID;
+                
+                // add ancestors
+                while ($parent_post->post_parent) {
+                    $parent_post = get_post($parent_post->post_parent);
+                    array_splice($crumbs, $index, 0, array(array(
+                        'id' => $parent_post->ID,
+                    )));
+                }
+                
+                break;
+            }
+            return $crumbs;
+        }, 10);
+    }
+    
+    private $post_type;
+    private $post_type_object;
+    private $option_name;
+    private $parent = null;
+    
+    private function __construct($post_type, WP_Post_Type $post_type_object)
+    {
+        $this->post_type = $post_type;
+        $this->post_type_object = $post_type_object;
+        $this->option_name = sprintf(self::OPTION_FORMAT, $post_type);
+        if (!empty($post_type_object->rewrite['parent'])) {
+            $this->parent = $post_type_object->rewrite['parent'];
+        }
+        
+        // display parent page field on reading settings page
+        add_action('admin_init', function () {
+            register_setting('reading', $this->option_name, array(
+                'type' => 'integer',
+            ));
+            add_settings_field(
+                $this->option_name,
+                '<label for="' . $this->option_name . '">' . sprintf(__('Parent page for %s type', 'cpt-parent'), $this->post_type_object->label) . '</label>',
+                function () {
+                    echo wp_dropdown_pages(array(
+                        'name'             => $this->option_name,
+                        'id'               => $this->option_name,
+                        'echo'             => 0,
+                        'show_option_none' => __('— Select —', 'cpt-parent'),
+                        'selected'         => get_option($this->option_name)
+                    ));
+                },
+                'reading'
+            );
+        });
+
+        // update post type and flush rewrite rules when parent changes
+        add_action('update_option_' . $this->option_name, function ($old_value, $value) {
+            $this->parent = $value;
+            $this->rewriteRules();
+        }, 10, 2);
+        
+        
+        if ($this->parent) {
+            
+            add_filter('the_title', function ($title, $id) {
+                global $wp_query;
+                if ($wp_query->is_main_query && $wp_query->is_archive && $wp_query->get('post_type') === $this->post_type) {
+                    $parent = get_post($this->parent);
+                    if ($parent) {
+                        $title = $parent->post_title;
+                    }
+                }
+                return $title;
+            }, 10, 2);
+            
+            // add page edit link in CPT menu
+            add_action('admin_menu', function () {
+                global $submenu;
+                $slug = 'edit.php?post_type='. $this->post_type;
+                if (isset($submenu[$slug])) {
+                    $submenu[$slug][] = array(
+                        __('Archive page', 'cpt-parent'),
+                        'edit_posts',
+                        get_admin_url(null, sprintf('post.php?post=%s&action=edit', $this->parent))
+                    );
+                }
+            });
+            
+            // display notice on edit screen
+            add_action('edit_form_after_title', function ($post) {
+                $post_ids = $this->getParentIds();
+                if (in_array($post->ID, $post_ids)) {
+                    echo '<div class="notice notice-warning inline"><p>' .
+                        sprintf(__('You are currently editing the page that shows your latest %s.', 'cpt-parent'), strtolower($this->post_type_object->label)) .
+                        '</p></div>';
+                }
+            });
+
+            add_action('save_post', function ($post_id) {
+                $post_ids = $this->getParentIds();
+                if (in_array($post_id, $post_ids)) {
+                    $old = get_post();
+                    $new = get_post($post_id);
+                    if ($old->post_name != $new->post_name) {
+                        $this->rewriteRules();
+                    }
+                }
+            });
+
+            // add ancestor classes, lost because archive entry is also a page
+            add_filter('nav_menu_css_class', function ($classes, $item) {
+                if ($item->type === 'post_type_archive' && $item->object === $this->post_type) {
+                    $post = get_post();
+                    if ($post && $post->post_type === $this->post_type) {
+                        $classes[] = 'current_page_ancestor';
+                        $classes[] = 'current-page-ancestor';
+                        $classes[] = 'current-menu-ancestor';
+                    }
+                }
+                return $classes;
+            }, 10, 2);
+            
+            // add ACF page type
+            add_filter('acf/location/rule_values/page_type', function ($values) {
+                $values[$this->post_type] = sprintf(__('Page of %s', 'cpt-parent'), strtolower($this->post_type_object->label));
+                return $values;
+            });
+            add_filter('acf/location/rule_match/page_type', function ($result, $rule, $screen) {
+                $post_ids = $this->getParentIds();
+                if ($rule['value'] === $this->post_type && !empty($screen['post_id'])) {
+                    if ($rule['operator'] === '==' && in_array($screen['post_id'], $post_ids)) {
+                        return true;
+                    }
+                    if ($rule['operator'] === '!=' && !in_array($screen['post_id'], $post_ids)) {
+                        return true;
+                    }
+                    return false;
+                }
+                return $result;
+            }, 10, 3);
+            
+            // translate rewrite rules
+            /*
+             * Should let translation plugin handle that.
+             * But keep the logic here, it works
+             * 
+            if (function_exists('pll_languages_list')) {
+                add_filter('rewrite_rules_array', function ($rules) {
+                    $languages = pll_languages_list();
+                    if (count($languages) < 2) {
+                        return $rules;
+                    }
+                    
+                    $baseSlug = $this->post_type_object->rewrite['slug'];
+
+                    $slugs = [$baseSlug];
+                    foreach ($languages as $lang) {
+                        $parent = pll_get_post($this->parent, $lang);
+                        $slug = get_page_uri($parent);
+                        if ($slug !== $baseSlug) {
+                            $slugs[] = $slug;
+                        }
+                    }
+
+                    $newRules = [];
+                    foreach ($rules as $key => $rule) {
+                        // if rule contains base slug
+                        if (strpos($key, $baseSlug) !== false) {
+                            // we add slug language variations
+                            $newRules[str_replace(
+                                $baseSlug,
+                                '(?:'. implode('|', $slugs) .')',
+                                $key
+                            )] = $rule;
+                        }
+                        else {
+                            // keep original rule
+                            $newRules[$key] = $rule;
+                        }
+                    }
+                    return $newRules;
+                });
+            }
+            */
+        }
+    }
+
+    private function getParentIds()
+    {
+        if (function_exists('pll_get_post_translations')) {
+            $post_ids = pll_get_post_translations($this->parent);
+        } else {
+            $post_ids = [$this->parent];
+        }
+        return $post_ids;
+    }
+    
+    private function rewriteRules()
+    {
+        // force post type update before flush rewrite rules
+        register_post_type($this->post_type, self::$post_type_args[$this->post_type]);
+        flush_rewrite_rules();
+    }
+}
